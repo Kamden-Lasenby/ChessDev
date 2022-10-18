7ce90b38892d8280d5a695d7a0b25f9e1eaf4f03
Kamden-Riley <63305712+Kamden-Riley@users.noreply.github.com>
2022-10-18 16:11:37 -0500

    Set and Clear Bits Part 10
    added functionality to set bits at U64 index and to also clear then inversely

--- init.c
+++ init.c
@@ -5,6 +5,28 @@
 int Sq120ToSq64[BRD_SQ_NUM];
 int Sq64ToSq120[64];
 
+U64 SetMask[64];
+U64 ClearMask[64];
+
+
+//this will initalize our setmask and clearmask arrays
+//it will also run a loop to shift a bit across the whole array and inverse for clearmask
+//in the MACROS the SETBIT will set a single bit at an index
+void InitBitMasks() {
+	int index = 0;
+	
+	for(index = 0; index < 64; index++) {
+		SetMask[index] = 0ULL;
+		ClearMask[index] = 0ULL;
+	}
+	
+	for(index = 0; index < 64; index++) {
+		SetMask[index] |= (1ULL << index);
+		ClearMask[index] = ~SetMask[index];
+	}
+	
+}
+
 void InitSq120To64() {
 	int index = 0;
 	int file = FILE_A;
@@ -32,4 +54,5 @@ void InitSq120To64() {
 void AllInit() {
 	
 	InitSq120To64();
+	InitBitMasks();
 }
