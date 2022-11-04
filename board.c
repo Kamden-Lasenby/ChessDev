c561aa7a6ba16375735cc06ae83feaeca8035580
Kamden-Riley <63305712+Kamden-Riley@users.noreply.github.com>
2022-11-04 16:23:36 -0500

    Parsing a FEN Record | Part (14-15)
    in these parts we learn what a FEN record is and write a function to parse and assemble board positions

--- board.c
+++ board.c
@@ -4,6 +4,90 @@
 #include "defs.h"
 
 
+// very important function that parses a Forsyth-Edwards Notation (FEN) record.
+// a FEN record is a notation style for describing a board position
+// a sample (starting) record looks like this: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+// the function parses character by character and assembles our chess board.
+int Parse_Fen(char *fen, S_BOARD *pos) {
+	
+	ASSERT(fen!=NULL);
+	ASSERT(pos!=NULL);
+	
+	int rank = RANK_8;
+	int file = FILE_A;
+	int piece = 0;
+	int count = 0; //empty squares
+	int i = 0; // for loop index
+	int sq64;
+	int sq120;
+	
+	ResetBoard(pos);
+	
+	while ((rank >= RANK_1) && *fen) {
+		count = 1;
+		switch (*fen) {
+			case 'p': piece = bP; break;
+			case 'r': piece = bR; break;
+			case 'n': piece = bN; break;
+			case 'b': piece = bB; break;
+			case 'k': piece = bK; break;
+			case 'q': piece = bQ; break;
+			case 'P': piece = wP; break;
+			case 'R': piece = wR; break;
+			case 'N': piece = wN; break;
+			case 'B': piece = wB; break;
+			case 'K': piece = wK; break;
+			case 'Q': piece = wQ; break;
+			
+			/* these cases convert empty spaces to strings
+			   and then assign them to the count variable
+			*/
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+				piece = EMPTY;
+				count = *fen - '0';
+				break;
+				
+			case '/':
+			case ' ':
+				rank--;
+				file = FILE_A;
+				fen++;
+				continue;
+			
+			default:
+				printf("FEN error \n");
+				return -1;
+		}
+		
+		/* this loop essentially uses the information parsed by the switch statement
+		   to assign the pieces in the FEN to our board.
+		   a cool feature of this is that count variable, which by default is equal to 1,
+		   making the loop only have to run once and places the parsed piece on the board.
+		   if the count is larger than one (explained in the last comment) then the loop will
+		   run that many times ignoring the piece assignment operation in the if-statement.
+		   small detail of the function but I thought it was cool.
+		*/
+		for (i = 0; i < count; i++) {
+			sq64 = rank * 8 + file;
+			sq120 = SQ120(sq64);
+			if (piece != EMPTY) {
+				pos->pieces[sq120] = piece;
+			}
+			file++;
+		}
+		
+		fen++; //increase the fen pointer to next character to parse
+	}
+	
+}
+
 void ResetBoard(S_BOARD *pos) {
 	
 	int index = 0;
@@ -47,19 +131,5 @@ void ResetBoard(S_BOARD *pos) {
 	pos->castlePerm = 0;
 	
 	pos->posKey = 0ULL;
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
